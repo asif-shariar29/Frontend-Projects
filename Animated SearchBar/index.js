cc79e7d5d335e2513af1d704539a5410841713ec
Asif Shariar <shariarasif0@gmail.com>
2025-08-31 15:28:48 +0600

    Delete Animated SearchBar directory

--- Animated SearchBar/index.js
+++
@@ -1,7 +0,0 @@
-const boxE1 = document.querySelector(".searchBar");
-const searchE1 = document.querySelector(".search");
-
-
-searchE1.addEventListener("click", ()=> {
-        boxE1.classList.toggle("active");
-});
